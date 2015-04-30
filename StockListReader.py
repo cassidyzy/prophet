d4bc1b6216893f384a493c18f930789ec0e38a70
root <root@controler2.crl.ibm.com>
2015-04-30 12:47:36 +0800

    MOD: change wb to rb

--- StockListReader.py
+++ StockListReader.py
@@ -1,5 +1,10 @@
 import pickle
 
 def read_stock_list():
-    fi = open("stock-list.txt", "wb")
+    fi = open("stock-list.txt", "rb")
     return pickle.load(fi)
+
+if __name__ == "__main__":
+    stock_list = read_stock_list()
+    for s in stock_list:
+        print(s)
