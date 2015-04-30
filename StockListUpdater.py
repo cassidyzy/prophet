725451a8e4d7c8948d1b8a30034feae66b4e08ec
wangxu <xuwang200985@sohu-inc.com>
2015-04-30 11:41:41 +0800

    ADD: first add

---
+++ StockListUpdater.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    print("update stock list start:")
+    sina_stock_list_url = "http://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHQNodeData?page=%d&num=%d&sort=symbol&asc=1&node=hs_a&symbol=&_s_r_a=page"
