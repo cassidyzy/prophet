529cba91374045cbf54962ac927796bcf18e77f6
wangxu <xuwang200985@sohu-inc.com>
2015-05-04 16:33:26 +0800

    MOD: add charset to crawler :

--- utils/crawler.py
+++ utils/crawler.py
@@ -3,14 +3,14 @@ from urllib.request import Request
 import urllib.request
 
 
-def crawl(url, timeout=10):
+def crawl(url, charset="gbk", timeout=10):
     request = Request(url, headers={'Accept-Encoding' : 'compress, gzip'})
     try:
         response = urllib.request.urlopen(request, timeout=timeout)
         if response.getheader("Content-Encoding") == "gzip":
-            return gzip.decompress(response.read()).decode("gbk")
+            return gzip.decompress(response.read()).decode(charset)
         else:
-            return response.read().decode("gbk")
+            return response.read().decode(charset)
     except:
         print("error for url %s" % url)
         return None
