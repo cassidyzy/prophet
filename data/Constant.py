5357bcba4292796e144fae35aadf650e5d983b0d
root <root@controler2.crl.ibm.com>
2015-05-14 14:01:16 +0800

    MOD: data for 2015-05-14

--- data/Constant.py
+++ data/Constant.py
@@ -1,6 +1,6 @@
 import re
 
 class ConstantValue:
-    DATE_SET = ["2015-05-13"]
+    DATE_SET = ["2015-05-14"]
     #DATE_SET = ["2015-05-13", "2015-05-12", "2015-05-11", "2015-05-08"]
     HOME_DIR = "./"
