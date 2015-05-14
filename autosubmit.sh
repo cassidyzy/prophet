ed03110a4fb2327e8774aa8d99f716db2fa46c7e
risune <risune@163.com>
2015-05-14 14:39:46 +0800

    FIX: update code before exec

--- autosubmit.sh
+++ autosubmit.sh
@@ -7,8 +7,8 @@ WORK_HOME=$HOME/prophet
 sh $HOME/.bash_profile
 
 cd $WORK_HOME
+git pull --rebase
 $PYTHON_HOME/bin/python3 StockMoneyFlowCollector.py
 git add results
 git commit -m "MOD: update daily money flow data"
-git pull --rebase
 git push
