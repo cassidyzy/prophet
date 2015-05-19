2b3519f2457d145be803dd32a52fb6c15a1dade5
wangxu <xuwang200985@sohu-inc.com>
2015-05-19 17:56:36 +0800

    ADD: pb rank utils

---
+++ PBRank.py
@@ -0,0 +1,17 @@
+import StockListReader
+
+if __name__ == "__main__":
+    stocks = StockListReader.read_stock_list()
+    money_flows = StockListReader.read_cur_money_flow()
+    pb = []
+    for stock in stocks:
+        if stock.symbol not in money_flows:
+            continue
+        money_flow = money_flows[stock.symbol]
+        if money_flow is None or money_flow.isChuangye():
+            continue
+        pb.append((stock, money_flow.trade / stock.net_estate))
+    pb = sorted(pb, key=lambda k : k[1])
+    for s in pb:
+        if s[1] > 0:
+            print("%s\t%f\t%s" % (s[0].symbol, s[1], s[0].name))
--- StockListReader.py
+++ StockListReader.py
@@ -1,12 +1,37 @@
 #!/usr/bin/python3
 
+import datetime
+import os
 import pickle
+import re
+
 
 def read_stock_list():
     fi = open("stock-list.txt", "rb")
     return pickle.load(fi)
 
-if __name__ == "__main__":
-    stock_list = read_stock_list()
-    for s in stock_list:
-        print(s)
+def read_cur_money_flow():
+    data_dir = "results/moneyflowdata"
+    # get most recent date
+    max_date = None
+    for fn in os.listdir(data_dir):
+        m = re.match(r'stock-money-flow-(.*)', fn)
+        if m:
+            d = datetime.datetime.strptime(m.group(1), '%Y-%m-%d')
+            if max_date is None:
+                max_date = d
+                selected = fn
+            else:
+                if d > max_date:
+                    max_date = d
+                    selected = fn
+    if selected is None:
+        print("sb le!, please quit!")
+        return None
+    
+    # read data in map, key is symbol, value is obj
+    result = {}
+    l = pickle.load(open("%s/%s" % (data_dir, selected), "rb"))
+    for s in l:
+        result[s.symbol] = s
+    return result
