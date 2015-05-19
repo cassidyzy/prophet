95c8a34f7b2cff9503c7e9b3d6f7b8af624faa82
wangxu <xuwang200985@sohu-inc.com>
2015-05-19 18:08:27 +0800

    FIX: divide by 0 bug

--- PBRank.py
+++ PBRank.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
         if stock.symbol not in money_flows:
             continue
         money_flow = money_flows[stock.symbol]
-        if money_flow is None or money_flow.isChuangye():
+        if money_flow.isChuangye() or stock.net_estate == 0:
             continue
-        pb.append((stock, money_flow.trade / stock.net_estate))
+        pb.append((stock, money_flow.trade / stock.net_estate, money_flow))
     pb = sorted(pb, key=lambda k : k[1])
     for s in pb:
         if s[1] > 0:
-            print("%s\t%f\t%s" % (s[0].symbol, s[1], s[0].name))
+            print("%s\t%f\t%s(%s,%s)" % (s[0].symbol, s[1], s[0].name, s[0].net_estate, s[2].trade))
